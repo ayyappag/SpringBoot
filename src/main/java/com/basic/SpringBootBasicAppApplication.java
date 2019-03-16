2265acde163c21af51459c5adbd3aa2bdd907b05
ayyappag <ayyappag@gmail.com>
2019-03-17 00:16:16 +0530

    Added
    Added

---
+++ src/main/java/com/basic/SpringBootBasicAppApplication.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("welcome ");
+		SpringApplication.run(SpringBootBasicAppApplication.class, args);
+	}
+
+}
